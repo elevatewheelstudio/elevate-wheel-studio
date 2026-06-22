6222757d241ddca975243fb82b99219a6642f26c
elevatewheelstudio <ISHMIL90@GMAIL.COM>
2026-06-22 12:32:45 -0400

    Create Home component for main page

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{
+      padding: 40,
+      fontFamily: "Arial",
+      background: "#0a0a0a",
+      color: "white",
+      minHeight: "100vh"
+    }}>
+      <h1>Elevate Wheel Studio</h1>
+      <p>Booking system is live</p>
+    </div>
+  );
+}
