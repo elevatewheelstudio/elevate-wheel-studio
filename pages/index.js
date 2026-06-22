f781a73c0a12303d29acb1e07c791ca00b8d79da
elevatewheelstudio <ISHMIL90@GMAIL.COM>
2026-06-22 12:38:29 -0400

    Create Home component for Elevate Wheel Studio

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{
+      padding: 40,
+      fontFamily: "Arial"
+    }}>
+      <h1>Elevate Wheel Studio</h1>
+      <p>System is now correctly structured</p>
+    </div>
+  );
+}
